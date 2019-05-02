1c125f75c6c0cdbd01bc9d9c0cda99101d6fb372
ppolyzos <ppolyzos@gmail.com>
2019-05-02 21:33:26 +0300

    feat(emoji): prepare libraries for emoji association

---
+++ shared/models/faces.ts
@@ -0,0 +1,36 @@
+import { MOJIS } from "./mojis";
+import { Rect } from "./rect";
+import * as emoji from 'emoji-dictionary';
+
+export class Face {
+  emotivePoint;
+  faceRectangle;
+  moji;
+  mojiIcon;
+
+  constructor(emotivePoint, faceRectangle: Rect) {
+    this.emotivePoint = emotivePoint;
+    this.faceRectangle = faceRectangle;
+    this.moji = this.chooseMoji(this.emotivePoint);
+    this.mojiIcon = this.moji.emojiIcon;
+  }
+
+  get mojiName() {
+    // Given an emoji like 😴 returns the word version, like 'sleepy'
+    return emoji.getName(this.mojiIcon);
+  }
+
+  chooseMoji(point) {
+    let closestMoji = null;
+    let closestDistance = Number.MAX_VALUE;
+    for (let moji of MOJIS) {
+      let emoPoint = moji.emotiveValues;
+      let distance = emoPoint.distance(point);
+      if (distance < closestDistance) {
+        closestMoji = moji;
+        closestDistance = distance;
+      }
+    }
+    return closestMoji;
+  }
+}
---
+++ types/emoji-dictionary.d.ts
@@ -0,0 +1,4 @@
+export function getName(unicodeChar: any): any;
+export function getUnicode(name: any): any;
+export const names: string[];
+export const unicode: string[];
